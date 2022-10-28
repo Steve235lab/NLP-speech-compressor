fe6d67e659baf7bda4741e02434654735a951f69
Steve D. J <84221016+Steve235lab@users.noreply.github.com>
2022-10-29 00:44:09 +0800

    PyQt桌面应用打包

--- src/Qt_main.py
+++ src/Qt_main.py
@@ -1,6 +1,21 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
+# 以下引用方便打包
+import api_info
+import app_core
+import compressor
+import local_audio_input
+import local_main
+import process_result_dict
+import Qt_CompressButton
+import Qt_CompressedText
+import Qt_OriginalText
+import Qt_SwitchButton
+import rank_bm25
+import rtasr_client
+import speech2text
+
 
 # 建立图形窗口
 Form, Window = uic.loadUiType("main_window.ui")
